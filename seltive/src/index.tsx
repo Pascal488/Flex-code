fca184fbc6563b7d1f6b507d9195a679090f31dd
Pascal488 <maxpascal03@gmail.com>
2022-11-02 22:50:30 +0300

    Font family added

--- seltive/src/index.tsx
+++ seltive/src/index.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React,{useState} from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "@fontsource/barlow";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,10 +15,11 @@ const client = new ApolloClient({
   uri: process.env.REACT_APP_API_URL,
   cache: new InMemoryCache(),
 });
+
 root.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>
+  
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+   
 );
