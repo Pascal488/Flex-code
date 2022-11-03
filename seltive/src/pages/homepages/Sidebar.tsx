6eb5242514ec5c7ef90f0acbef03f21d4a30d8cc
Pascal488 <maxpascal03@gmail.com>
2022-11-04 02:21:20 +0300

    Adding icons

--- seltive/src/pages/homepages/Sidebar.tsx
+++ seltive/src/pages/homepages/Sidebar.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import {AiOutlineHome} from 'react-icons/ai'
+import {HiOutlineCog} from 'react-icons/hi'
+import {TbChartPie} from 'react-icons/tb'
+import {BsSearch} from 'react-icons/bs'
+import {HiOutlineFolder} from 'react-icons/hi'
+import {BsBookmark} from 'react-icons/bs'
+import {HiOutlineReceiptRefund} from 'react-icons/hi'
+
+
+
 
 const Sidebar = () => {
   return (
     <div>
-        <h1 className='text-center text-sky-500'>
-            Your in now
+        <h1 className='text-center text-sky-500 text-2xl p-4'>
+            <AiOutlineHome/>
+            <HiOutlineCog/>
+            <TbChartPie/>
+            <BsSearch/>
+            <HiOutlineFolder/>
+            <BsBookmark/>
+            <HiOutlineReceiptRefund/>
         </h1>
     </div>
   )
