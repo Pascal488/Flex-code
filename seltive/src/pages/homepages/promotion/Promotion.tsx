fdbbb58eb7a038d636731dfe5279eb3179a99723
Pascal488 <maxpascal03@gmail.com>
2022-11-20 10:32:27 +0300

    :Fix dark-mode

--- seltive/src/pages/homepages/promotion/Promotion.tsx
+++ seltive/src/pages/homepages/promotion/Promotion.tsx
@@ -15,7 +15,6 @@ const Promotion = () => {
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
-      backgroundColor: "white",
       width: "416px",
       gap: "12px",
       padding: "12px",
@@ -31,21 +30,22 @@ const Promotion = () => {
       borderRadius: "5px",
     },
     honeelement: {
-      color: "rgb(75 85 99 / var(--tw-text-opacity))",
       marginBottom: "4px",
       fontWeight: "600",
     },
   };
   return (
     <div className="p-5">
-      <h1 className="text-gray-700 font-semibold text-2xl">Promotion</h1>
-      <p className="mb-10 text-gray-500">
+      <h1 className="text-gray-700 font-semibold text-2xl dark:text-blue-400">
+        Promotion
+      </h1>
+      <p className="mb-10 text-gray-500 dark:text-gray-400 ">
         Setup promotions for your customers, either on the entire store or
         specific products
       </p>
       <div className="flex  flex-wrap gap-5 max-w-5xl ">
         <div
-          className="shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
+          className="dark:bg-slate-900 dark:text-blue-500 bg-white shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
           style={styles.outer}
         >
           <div style={styles.innner} className="bg-red-200">
@@ -55,7 +55,7 @@ const Promotion = () => {
             <h1 style={styles.honeelement}>
               Contest, Sweepstakes, or giveaways
             </h1>
-            <p className="max-w-[300px] text-xs text-gray-500 font-extrabold">
+            <p className="max-w-[300px] text-xs text-gray-500  dark:text-gray-400 font-extrabold">
               Run a giveaway contest to your customers for a chance to win your
               product or products as prize.
             </p>
@@ -63,7 +63,7 @@ const Promotion = () => {
         </div>
 
         <div
-          className="shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
+          className=" dark:bg-slate-900 dark:text-blue-500 bg-white shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
           style={styles.outer}
         >
           <div style={styles.innner} className="bg-indigo-200">
@@ -71,14 +71,14 @@ const Promotion = () => {
           </div>
           <div>
             <h1 style={styles.honeelement}>Flash Sale</h1>
-            <p className="max-w-[300px] text-xs text-gray-500 font-extrabold">
+            <p className="max-w-[300px] text-xs text-gray-500 dark:text-gray-400 font-extrabold">
               Provide a high value product at a discount price for a limited
               amount of time.
             </p>
           </div>
         </div>
         <div
-          className="shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
+          className=" dark:bg-slate-900 dark:text-blue-500 bg-white shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
           style={styles.outer}
         >
           <div style={styles.innner} className="bg-green-200">
@@ -86,14 +86,14 @@ const Promotion = () => {
           </div>
           <div>
             <h1 style={styles.honeelement}>Free or Low-priced Samples</h1>
-            <p className="max-w-[300px] text-xs text-gray-500 font-extrabold">
+            <p className="max-w-[300px] text-xs text-gray-500 dark:text-gray-400 font-extrabold">
               Offer free samples or low-priced samples of your premium product
               to potential customers.
             </p>
           </div>
         </div>
         <div
-          className="shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
+          className=" dark:bg-slate-900 dark:text-blue-500 bg-white shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
           style={styles.outer}
         >
           <div style={styles.innner} className="bg-yellow-200">
@@ -101,13 +101,13 @@ const Promotion = () => {
           </div>
           <div>
             <h1 style={styles.honeelement}>Deals</h1>
-            <p className="max-w-[300px] text-xs text-gray-500 font-extrabold">
+            <p className="max-w-[300px] text-xs text-gray-500 dark:text-gray-400 font-extrabold">
               Offer great deals to your cutomers and increase your purchases.
             </p>
           </div>
         </div>
         <div
-          className="shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
+          className=" dark:bg-slate-900 dark:text-blue-500 bg-white shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
           style={styles.outer}
         >
           <div style={styles.innner} className="bg-gray-200">
@@ -115,14 +115,14 @@ const Promotion = () => {
           </div>
           <div>
             <h1 style={styles.honeelement}>Exclusive Cross-Sale</h1>
-            <p className="max-w-[300px] text-xs text-gray-500 font-extrabold">
+            <p className="max-w-[300px] text-xs text-gray-500 dark:text-gray-400 font-extrabold">
               Offer an exclusive product that is only available when a customer
               purchase something specific.
             </p>
           </div>
         </div>
         <div
-          className="shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
+          className=" dark:bg-slate-900 dark:text-blue-500 bg-white shadow-sm transition ease-in-out delay-150 hover:scale-105 hover:duration-300 cursor-pointer"
           style={styles.outer}
         >
           <div style={styles.innner} className="bg-red-200">
@@ -130,7 +130,7 @@ const Promotion = () => {
           </div>
           <div>
             <h1 style={styles.honeelement}>Coupons or Discounts</h1>
-            <p className="max-w-[300px] text-xs text-gray-500 font-extrabold">
+            <p className="max-w-[300px] text-xs text-gray-500 dark:text-gray-400 font-extrabold">
               Set up expiring coupons that are delivered to prospects or
               customers at strategic times
             </p>
