814bd62d96264d0a56a6dd50a700ecafe7ac87e2
Pascal488 <maxpascal03@gmail.com>
2022-11-21 10:13:16 +0300

    :Fix ordertable

--- seltive/src/pages/homepages/orders/Ordertable.tsx
+++ seltive/src/pages/homepages/orders/Ordertable.tsx
@@ -1,8 +1,7 @@
-import React,{useMemo} from 'react'
+import React, { useMemo } from "react";
 import { useTable } from "react-table";
 import { COLUMNS } from "./OrderColumns";
 import Dummy from "./Dummy.json";
-
 
 const Ordertable = () => {
   const columns = useMemo(() => COLUMNS, []);
@@ -13,37 +12,50 @@ const Ordertable = () => {
   });
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-  tableInstance;
+    tableInstance;
 
- 
   return (
     <div>
-
-<table {...getTableProps()}>
-      <thead >
-        {headerGroups.map((headerGroups) => (
-          <tr {...headerGroups.getHeaderGroupProps()} >
-            {headerGroups.headers.map((columns) => (
-              <th {...columns.getHeaderProps()} className='py-[8px] px-[30px] text-left'>{columns.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((rows) => {
-          prepareRow(rows);
-          return (
-            <tr {...rows.getRowProps()} className="hover:color-indigo-600 dark:hover:bg-slate-900 hover:font-bold">
-              {rows.cells.map((cell) => {
-                return <td {...cell.getCellProps()} className='py-[8px] px-[30px] text-left'>{cell.render("Cell")}</td>;
-              })}
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroups) => (
+            <tr {...headerGroups.getHeaderGroupProps()}>
+              {headerGroups.headers.map((columns) => (
+                <th
+                  {...columns.getHeaderProps()}
+                  className="py-[8px] px-[30px] text-left"
+                >
+                  {columns.render("Header")}
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((rows) => {
+            prepareRow(rows);
+            return (
+              <tr
+                {...rows.getRowProps()}
+                className="hover:color-indigo-600 dark:hover:bg-slate-900 hover:font-bold"
+              >
+                {rows.cells.map((cell) => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      className="py-[8px] px-[30px] text-left"
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Ordertable
+export default Ordertable;
