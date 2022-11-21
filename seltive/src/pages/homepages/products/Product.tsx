4515710876113093a475a45acca1f60ccab8a6a7
Pascal488 <maxpascal03@gmail.com>
2022-11-21 20:40:44 +0300

    :Add product page desgn

--- seltive/src/pages/homepages/products/Product.tsx
+++ seltive/src/pages/homepages/products/Product.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 
+import {BiPlus} from "react-icons/bi"
+
 const Product = () => {
   return (
-
-      <div className="flex justify-between items-center   mb-2">
+      <>
+      <div className="flex justify-between items-center mb-2 min-w-[1200px] " >
         <div>
           <h1 className="text-3xl mt-2 dark:text-indigo-600 dark:font-semibold">
             Products
           </h1>
           <span className="text-gray-400">
-            View all your store’s products catalogue
+            View all your store’s products catalogue 
           </span>
         </div>
-
-        <div className="justify-self-end">
-            <span>
-                <input type="text" className="bg-blue-200 w-3/4" />
-            </span>
+        <div className="flex  items-center justify-center w-[17%] text-white bg-indigo-600 gap-1 p-1.5 rounded-sm cursor-pointer">
+        <BiPlus className="text-xl"/>
+        <input type="button" value="Create New Product" className="cursor-pointer" />
         </div>
       </div>
-  
+      </>
   );
 };
 
