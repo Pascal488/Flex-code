3a5d81f1637e1aa078c6b7fb7aaed1a156525666
Pascal488 <maxpascal03@gmail.com>
2022-11-24 13:01:24 +0300

    :Fix logo

--- seltive/src/pages/homepages/Sidebar.tsx
+++ seltive/src/pages/homepages/Sidebar.tsx
@@ -51,7 +51,10 @@ const Sidebar = () => {
   return (
     <div className="flex flex-col p-4 gap-2 shadow-xl h-screen dark:bg-slate-900 ">
       <div className="flex  gap-4 items-center mb-10">
-        <span className="w-10 h-10 rounded-full bg-indigo-600"></span>
+      <span className="relative md:hidden flex flex-col gap-2 w-10 h-10 rounded-full bg-indigo-600">
+            <span className="absolute bottom-5 left-1  rotate-[145deg] w-[30px] p-[3px] rounded-md h-[2px] bg-green-200 "></span>
+            <span className="absolute  bottom-3 left-2   rotate-[145deg] w-[29px] p-[3px]  rounded-md h-[2px] bg-red-200"></span>
+          </span>
 
         <h1 className=" text-2xl text-indigo-700 font-extrabold">Seltive</h1>
       </div>
