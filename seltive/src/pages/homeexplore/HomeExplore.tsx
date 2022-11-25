cf2cf905f1b33df361e0e8b11b0498f11429c073
Pascal488 <maxpascal03@gmail.com>
2022-11-25 10:40:26 +0300

    :Fix responsive homeexplore page

--- seltive/src/pages/homeexplore/HomeExplore.tsx
+++ seltive/src/pages/homeexplore/HomeExplore.tsx
@@ -20,21 +20,19 @@ const HomeExplore = () => {
   };
   return (
     <>
-   
-    <Header/>
+      <Header />
 
-   
       <div className="p-5 w-full ">
-        <div className="ml-24">
+        <div className="ml-24 md:ml-0 ">
           <h1 className="text-gray-500 font-semibold ">EXPLORE SELTIVE</h1>
-          <p className="text-4xl md:text-sm mb-2">
+          <p className="text-4xl md:text-sm mb-2 md:max-w-[200px]">
             Discover the best content for entertainment, education,
             <br /> or assets for your next project.
           </p>
         </div>
 
-        <div className="flex items-center justify-around w-full gap-2  md:flex-col ">
-          <div className="flex flex-col p-5 bg-white shadow-md">
+        <div className="flex items-center justify-around w-full gap-2  md:flex-col md:justify-center ">
+          <div className="flex flex-col p-5 bg-white shadow-md md:w-[245px]">
             <h1>Animations</h1>
             <span>Movies, tutorials, rigs, and assets</span>
             <span style={commonstyles.container}>
@@ -42,7 +40,7 @@ const HomeExplore = () => {
               <BsArrowRightShort />
             </span>
           </div>
-          <div className="flex flex-col p-5 bg-white shadow-md">
+          <div className="flex flex-col p-5 bg-white shadow-md md:w-[245px]">
             <h1>Apps & Software</h1>
             <span>Plugins, Extentions, Source Code etc.</span>
             <span style={commonstyles.container}>
@@ -50,7 +48,7 @@ const HomeExplore = () => {
               <BsArrowRightShort />
             </span>
           </div>
-          <div className="flex flex-col p-5 bg-white shadow-md">
+          <div className="flex flex-col p-5 bg-white shadow-md md:w-[250px]">
             <h1>Books & Writing</h1>
             <span>Stories, Articles, Guides, etc.</span>
             <span style={commonstyles.container}>
@@ -60,24 +58,23 @@ const HomeExplore = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white w-full p-7 ">
-        <h1 className="ml-20 font-extrabold text-indigo-600">TRENDING CONTENT TODAY</h1>
-        <div >
-        <div className='flex flex-wrap justify-around ml-10 mt-4 gap-6 md:items-center  md:justify-center md:flex md:flex-col'>
-              <Card/> 
-              <Card/> 
-              <Card/> 
-              <Card/> 
-              <Card/> 
-              <Card/> 
-              <Card/> 
-              <Card/> 
-              <Card/> 
+      <div className="bg-white w-full p-7 md:p-0 ">
+        <h1 className="ml-20 md:p-2 md:text-center md:ml-0 font-extrabold text-indigo-600">
+          TRENDING CONTENT TODAY
+        </h1>
+
+        <div className="flex flex-wrap justify-around ml-10 md:ml-0  mt-4 gap-6 md:items-center  md:justify-center md:flex md:flex-col">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
       </div>
-      </div>
-
-
     </>
   );
 };
