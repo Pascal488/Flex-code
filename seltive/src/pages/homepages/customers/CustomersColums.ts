19b4108c0e94a8ecfbf41dd786f13be0ed61ce04
Pascal488 <maxpascal03@gmail.com>
2022-11-27 10:26:29 +0300

    :Add customers page init

---
+++ seltive/src/pages/homepages/customers/CustomersColums.ts
@@ -0,0 +1,33 @@
+export const COLUMNS = [
+    {
+        Header:'Customer Name',
+        accessor:"customer"
+    },
+    {
+        Header:'Email',
+        accessor:"email"
+    },
+
+    {
+        Header:'Items Purchased',
+        accessor:"items_purchased"
+    },
+    {
+        Header:'Total Value',
+        accessor:"total_value"
+    },
+
+    {
+        Header:'Date Modified',
+        accessor:"date"
+    },
+    {
+        Header:'Status',
+        accessor:"status"
+    },
+    {
+        Header:'',
+        accessor:"dots"
+    },
+   
+]
